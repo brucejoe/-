d25270d4c7a0ab7aed338a87e00ecdd75bcf96ae
hpc <1369682389@qq.com>
2018-03-16 11:33:10 +0800

    我的第一个页面

--- pages/first/first.js
+++ pages/first/first.js
@@ -1,6 +1,13 @@
-//获取应用实例
-const app = getApp();
-
 Page({
+  data: {
+    array: [{
+      pic: '小鹬1',
+      img:'https://img3.doubanio.com/view/photo/raw/public/p2373241302.jpg'
+    }, {
+      pic: '小鹬2',
+      img:'https://img3.doubanio.com/view/photo/raw/public/p2453399733.jpg'
+    }]
+  }
+
   
 })
